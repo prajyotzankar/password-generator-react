919c949fefd96fe37636e974a789641785e3f571
prajyotzankar <zankarprajyotsushil@gmail.com>
2022-09-16 02:46:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import PasswordGenerator from "./components/PasswordGenerator";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='container'>
+        <Routes>
+          <Route path="/" element={<PasswordGenerator />} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
